dcdef5f81a9534c05081190d9ccb81ca31c93590
yeul <mpscqw@gmail.com>
2022-07-11 10:51:23 -0400

    add instructions component and short term route

---
+++ src/components/Instructions.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	export let instructions = [];
+</script>
+
+<div class="content__row instructions">
+	<ol>
+		{#each instructions as instruction, index}
+			<li class="instructions__item-wrapper">
+				<div class="instructions__bullet">
+					<span>{index + 1}</span>
+				</div>
+				<div>{@html instruction}</div>
+			</li>
+		{/each}
+	</ol>
+</div>
+
+<style type="text/scss">
+	@import '../styles/components/_instructions.scss';
+</style>
