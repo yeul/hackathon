637881c62bac33e513f172e284265e1d9be3cb56
yeul <mpscqw@gmail.com>
2022-07-11 11:12:58 -0400

    support scss. add shortTerm html

--- src/components/Instructions.svelte
+++ src/components/Instructions.svelte
@@ -1,4 +1,4 @@
-<script lang="ts">
+<script lang="js">
 	export let instructions = [];
 </script>
 
---
+++ src/routes/ShortTerm/shortTerm.svelte
@@ -0,0 +1,22 @@
+<script lang="js">
+	import Instructions from '../../components/Instructions.svelte';
+
+  // TODO: temp static data for testing
+	const instructions = [
+		'Choose PayPal at checkout to pay later with <strong>Pay in 4</strong>.',
+		'Complete your purchase with a 25% down payment.',
+		'Use autopay for the rest of your payments. It’s easy!'
+	];
+</script>
+
+<div class="content__container">
+	<main class="main">
+		<div class="content__body">
+			<div class="content__row dynamic">
+				<div class="content__col">
+					<Instructions {instructions} />
+				</div>
+			</div>
+		</div>
+	</main>
+</div>
