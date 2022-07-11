cc776897166cf155ad29d0d2ab6b9857cc331f6b
Merlin (they/them) <421960+merlinpatt@users.noreply.github.com>
2022-07-11 11:24:33 -0400

    add gitignore and button

---
+++ src/components/Button.svelte
@@ -0,0 +1,17 @@
+<script>
+	let classes = ['button'];
+	export let secondary;
+	export let className;
+	export let type = 'button';
+	export let onClick = () => {};
+
+	if (className) classes = [...classes, className];
+</script>
+
+<button class={classes.join(' ')} class:secondary {type} on:click={onClick}>
+	<slot />
+</button>
+
+<style type="text/scss">
+	@import '../styles/components/_button.scss';
+</style>
